341adc91b273bbc7c881444979bb6f38061d789a
Eric <eric.snyder@tufts.edu>
2015-10-22 15:43:21 -0400

    Updated js file

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,20 @@
 // Your JavaScript goes here...
+//Job: to modify this to open up data.json using XTMLHttpRequest, parse JSON data, and output messages
+//      in the "messages" section of index.html, as ordered in JSON
+//
+// Messages in HTML looks like this :
+//
+//		<body onload="parse()">
+//			<h1>Messages</h1>
+//			<div id="messages"></div>
+//		</body>
+//
+function parse(){
+	
+	var request = XTMLHttpRequest;
+	var data;
+	var parseddata;
+	request.open("GET", "index.html", true);
+	request.send();
+
+};
